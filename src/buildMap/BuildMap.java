c2df3c02993c66b0ab8d9f3efec0132f8780c956
Mateusz Wietrak <wietrak11@gmail.com>
2019-03-21 18:30:24 +0100

    New panes in map build

--- src/buildMap/BuildMap.java
+++ src/buildMap/BuildMap.java
@@ -4,7 +4,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
